bc3e8555e35be243259a6a74611ac5ad9a6180d8
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-05-05 20:58:38 +0200

    reformat jest config

--- packages/nx-deno/jest.config.ts
+++ packages/nx-deno/jest.config.ts
@@ -1,14 +1,14 @@
 module.exports = {
-  displayName: 'nx-deno',
-  preset: '../../jest.preset.ts',
+  displayName: "nx-deno",
+  preset: "../../jest.preset.ts",
   globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
     },
   },
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    "^.+\\.[tj]s$": "ts-jest",
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/nx-deno',
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageDirectory: "../../coverage/packages/nx-deno",
 };
